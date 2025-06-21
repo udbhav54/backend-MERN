3a0c67ca743e21242020222097f92c3af7a2bd9c
Udbhav <udbhavkumar54@gmail.com>
2025-06-21 12:44:03 +0530

    util code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+// function declaration
+// 2nd approach(promises)
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+  }
+};
+
+export { asyncHandler }; // in this way we export function
+
+// high order function -> which take function as parameter or return as function
+
+// const asyncHandler = () => {}
+// const asyncHandler = (fun) => () => {};
+// const asyncHandler = () => async() => {};
+// we are making wrapper funtion (1st approach(using trycatch))
+
+// const asyncHandler = (fun) => async(req, res, next) => {
+//   try {
+//     await fun(req, res, next)
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message
+//     })
+//   }
+// }
